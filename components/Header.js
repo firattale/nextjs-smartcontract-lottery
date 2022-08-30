c4e5d32fa56c620df8f90818adc8f83793148ba9
Firat Tale <firat.tale@auto1.com>
2022-08-30 16:47:00 +0200

    feat: add tailwind

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import { ConnectButton } from "@web3uikit/web3";
 
 export default function Header() {
   return (
-    <header>
+    <header className="border-b-2 p-5 flex flex-row">
       Decentralized Lottery
       <ConnectButton moralisAuth={false} />
     </header>
